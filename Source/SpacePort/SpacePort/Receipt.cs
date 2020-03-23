5571401a9ecb11ce7722846e06258d977fb1451a
zorkii <micael.wollter@gmail.com>
2020-03-23 16:20:00 +0100

    Driver @ahozork

--- Source/SpacePort/SpacePort/Receipt.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SpacePort
-{
-    public class Receipt
-    {
-        public int ReceiptID { get; set; }
-        public int TicketID { get; set; }
-        public int TotalPrice { get; set; }
-    }
-}
