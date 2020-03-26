06d58cdc3f91112b500fb142e0784fe2dd680ae0
PontusGillenang <PontusGillenang@users.noreply.github.com>
2020-03-26 23:29:10 +0100

    Driver @pontusgillenang

--- Source/SpacePort/SpacePort/Menu.cs
+++ Source/SpacePort/SpacePort/Menu.cs
@@ -1,4 +1,4 @@
-﻿using SpacePort.Models;
+﻿using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 
@@ -8,11 +8,13 @@ namespace SpacePort
     {
         static StarWarsApi swAPI = new StarWarsApi();
         static Spaceport spacePort = new Spaceport();
+
+        static JObject jObject = new JObject();
+
+        static DateTime startTime;
+        static DateTime endTime;
         public static string CustomerName { get; private set; }
         public static int MenuChoice { get; private set; }
-
-        DateTime startTime;
-        DateTime endTime;
 
         public static void Display()
         {
@@ -25,49 +27,55 @@ namespace SpacePort
             Console.Write("\n\rPlease Make a choice: ");
             MenuChoice = int.Parse(Console.ReadLine());
 
-            if(MenuChoice == 1)
+            if (MenuChoice == 1)
             {
                 Console.Write("Please *BEEP* *BOOP* enter your name: ");
                 CustomerSignIn();
-                
-                bool isValidated  = ValidateCustomer();
-                if(isValidated == true)
-                    DisplayShipTypes();
-                else
+
+                bool valid = false;
+                while (valid == false)
                 {
                     Console.WriteLine("*BEEP* Do.Or do not.There is no try. *BOOP*");
-                    Console.Write("Your name please: ");
+                    Console.Write("Try again: ");
                     CustomerSignIn();
+                    valid = ValidateCustomer();
+
+                    if(valid == true)
+                    {
+                        break;
+                    }
                 }
 
                 Console.WriteLine("Please enter your time of arrival (YYYY/MM/DD HH:MM): ");
                 string arrivalInput = Console.ReadLine();
-                DateTime startTime = DateTime.Parse(arrivalInput);
+                InputDate(arrivalInput, startTime);
                 Console.WriteLine("Please enter your time of depature (YYYY/MM/DD HH:MM): ");
                 string depatureInput = Console.ReadLine();
-                DateTime endTime = DateTime.Parse(depatureInput);
+                InputDate(depatureInput, endTime);
             }
-            else if(MenuChoice == 2)
+            else if (MenuChoice == 2)
             {
                 //Checkout method called, and check in database if user is checked in
             }
-             
+
             //if starwarsperson not found: Retry and text: “Do. Or do not. There is no try.”
 
             //Checkin confirmation text: "The garbage’ll do!” 
             ///checkout text: "I find your lack of faith disturbing".
 
-
-
         }
 
-        
+        private static void InputDate(string input, DateTime dateTime)
+        {
+            dateTime = DateTime.Parse(input)
+            spaceport.GetDate(dateTime);
+        }
 
         private static bool ValidateCustomer()
         {
-            //return swAPI.GetTravelerAsync(CustomerName.ToLower()).Result; 
-
-            return true;
+            jObject = swAPI.GetTravelerDataAsync(CustomerName).Result;
+            bool isValid = swAPI.ValidateTraveler(jObject, CustomerName);
+            return isValid;
         }
 
         public static void DisplayShipTypes()
@@ -78,7 +86,7 @@ namespace SpacePort
 
         public static void CustomerSignIn()
         {
-            
+
             CustomerName = Console.ReadLine();
         }
     }
