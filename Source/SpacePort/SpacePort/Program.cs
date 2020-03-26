4f53a810e37b4d67f36c032d601fd9803d97e4f0
PontusGillenang <PontusGillenang@users.noreply.github.com>
2020-03-26 22:31:43 +0100

    Driver @pontusgillenang
    Some changes to menu.

--- Source/SpacePort/SpacePort/Program.cs
+++ Source/SpacePort/SpacePort/Program.cs
@@ -11,64 +11,10 @@ namespace SpacePort
     {
         static void Main(string[] args)
         {
-
-
             //Spaceport spacePort = new Spaceport();
             //spacePort.GetParkingLots();
-
-            //Menu.DisplayMenu();
-
-
+            //Menu.Display();
             Console.ReadKey();
-
-
-
-            //Console.ReadKey();
-
-            /*
-            Ticket ticket = new Ticket();
-            ticket.TimeOfArrival = new DateTime(2020, 03, 24, 15, 0, 0);
-            ticket.TimeOfDepature = new DateTime(2020, 03, 24, 15, 50, 0);
-
-            Console.WriteLine(ticket.ParkedTimeInMinutes());
-            Console.ReadKey();
-            */
-
-            //StarWarsApi api = new StarWarsApi();
-            //Dictionary<string, int> dict = api.GetAsyncShipData().Result;
-            //Dictionary<string, int> dict = new Dictionary<string, int>();
-
-            //string[] arr = { "Large", "Medium", "Small", "Small" };
-
-            //foreach (string s in arr)
-            //{
-            //    if (!dict.ContainsKey(s))
-            //        dict.Add(s, 10);
-            //}
-
-            //foreach (KeyValuePair<string, int> k in dict)
-            //    Console.WriteLine(k);
-
-            //Console.WriteLine("BUY");
-            //CheckInParking("Large", dict);
-            //CheckInParking("Large", dict);
-            //CheckInParking("Medium", dict);
-            //CheckInParking("Medium", dict);
-            //CheckInParking("Medium", dict);
-            //CheckInParking("Medium", dict);
-
-            //foreach (KeyValuePair<string, int> k in dict)
-            //    Console.WriteLine(k);
-
-            //Console.WriteLine("SELL");
-            //CheckOutParking("Medium", dict);
-            //CheckOutParking("Medium", dict);
-
-            //foreach (KeyValuePair<string, int> k in dict)
-            //    Console.WriteLine(k);
-
-
-
         }
 
         //async void WriteResult() { }
