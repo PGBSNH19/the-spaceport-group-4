bff4c5dd7e81140b149fff7893fad7b942f7afa7
samir19ehsani <samirehsani19@gmail.com>
2020-03-27 12:09:19 +0100

    Driver @samirehsani19
    Solved problem with dublicate receipt information

--- Source/SpacePort/SpacePort/Program.cs
+++ Source/SpacePort/SpacePort/Program.cs
@@ -11,12 +11,13 @@ namespace SpacePort
     {
         static void Main(string[] args)
         {
+             //pass these values.  #string name, string shipN, DateTime arrTime, DateTime dpTime, int price
             Spaceport s = new Spaceport();
 
-            // pass these values.  #string name, string shipN, DateTime arrTime, DateTime dpTime, int price
-            s.InsertDataToTciketDB("Mirko", "Ranger", Convert.ToDateTime("2020-03-26 12:30:00"), Convert.ToDateTime("2020-03-27 14:30:00"), 500);
-            s.DisplayReceipt("Mike"); // shows receipt by person name
-            s.RemovePerson("Mirko");  // remove someone from database
+           // s.InsertDataToTciketDB("Jahn", "Jet Fighter", Convert.ToDateTime("2020-03-26 12:30:00"), Convert.ToDateTime("2020-03-27 14:30:00"), 500);
+            Console.WriteLine("This Person Receipt Is Ready");  //comment out this code together with display receipt
+            s.DisplayReceipt("Jahn"); // shows receipt by person name
+            //s.RemovePerson("Samir");  // remove someone from database
             Console.ReadKey();
 
 
--- Source/SpacePort/SpacePort/Spaceport.cs
+++ Source/SpacePort/SpacePort/Spaceport.cs
@@ -109,31 +109,39 @@ namespace SpacePort
                 };
                 Context.Receipt.Add(receipt);
                 Context.SaveChanges();
-                Console.WriteLine("Data Inserted To Database Successfuly \n\n");
-                Console.WriteLine("This Data Inserted To Database");
-
-                var person = (from t in Context.Ticket
-                             join r in Context.Receipt
-                             on t.ID equals r.TicketID
-                             where (t.PersonName == name)
-                             select new
-                             { 
-                                 r.TicketID,
-                                 t.PersonName,
-                                 t.ShipName,
-                                 t.DateTimeOfArrival,
-                                 t.DateTimeOfDepature,
-                                 r.TotalPrice
-                             }).ToList();
-
-                Console.ForegroundColor = ConsoleColor.Blue;
-                foreach (var p in person)
+                Console.WriteLine("The Following Data Inserted To Database");
+                DisplayReceipt(name);
+/*
+                    checkName = (from p in Context.Ticket
+                                 where p.PersonName == name
+                                 orderby p.ID descending
+                                 select p).FirstOrDefault();
+                if (checkName.PersonName==name)
                 {
-                    Console.WriteLine(
-                        "Ticket ID:\t {0}\n Name:\t\t {1}\n Ship Name:\t {2}\n Arrival Date:\t {3}\n Departure Date: {4}\n Total Price:\t ${5}\n\n",
-                        p.TicketID, p.PersonName, p.ShipName, p.DateTimeOfArrival, p.DateTimeOfDepature, p.TotalPrice);
-                }
-                Console.ResetColor();
+                    var person = (from t in Context.Ticket
+                                  join r in Context.Receipt
+                                  on t.ID equals r.TicketID
+                                  where (t.PersonName == name)
+                                  orderby t.ID descending
+                                  select new
+                                  {
+                                      r.TicketID,
+                                      t.PersonName,
+                                      t.ShipName,
+                                      t.DateTimeOfArrival,
+                                      t.DateTimeOfDepature,
+                                      r.TotalPrice
+                                  }).ToList().FirstOrDefault();
+
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    foreach (var p in person)
+                    {
+                        Console.WriteLine(
+                            "Ticket ID:\t {0}\n Name:\t\t {1}\n Ship Name:\t {2}\n Arrival Date:\t {3}\n Departure Date: {4}\n Total Price:\t ${5}\n\n",
+                            p.TicketID, p.PersonName, p.ShipName, p.DateTimeOfArrival, p.DateTimeOfDepature, p.TotalPrice);
+                    }
+                    Console.ResetColor(); 
+                }*/
             }
             catch (Exception)
             {
@@ -141,13 +149,15 @@ namespace SpacePort
                 throw;
             }
         }
+        private Ticket checkName;
         public void DisplayReceipt(string name)
         {
             try
             {
-                var checkName = Context.Ticket
-                        .Where(person => person.PersonName == name)
-                        .SingleOrDefault();
+                 checkName = (from p in Context.Ticket
+                                 where p.PersonName == name
+                                 orderby p.ID descending
+                                 select p).FirstOrDefault();
 
                 if (checkName.PersonName == name)
                 {
@@ -155,6 +165,7 @@ namespace SpacePort
                                 join receipt in Context.Receipt
                                 on ticket.ID equals receipt.TicketID
                                 where (ticket.PersonName == name)
+                                orderby ticket.ID descending
                                 select new
                                 {
                                     receipt.TicketID,
@@ -163,35 +174,54 @@ namespace SpacePort
                                     ticket.DateTimeOfArrival,
                                     ticket.DateTimeOfDepature,
                                     receipt.TotalPrice
-                                }).ToList();
+                                }).ToList().FirstOrDefault();
 
-                    Console.WriteLine("This Person Receipt Is Ready");
+                    //Console.WriteLine("This Person Receipt Is Ready");
                     Console.ForegroundColor = ConsoleColor.Green;
-                    foreach (var p in data)
-                    {
-                        Console.WriteLine(
-                            "Ticket ID:\t {0}\n Name:\t\t {1}\n Ship Name:\t {2}\n Arrival Date:\t {3}\n Departure Date: {4}\n Total Price:\t ${5}\n",
-                            p.TicketID, p.PersonName, p.ShipName, p.DateTimeOfArrival, p.DateTimeOfDepature, p.TotalPrice);
-                    }
+                    Console.WriteLine(
+                        $"Ticket ID:\t{data.TicketID}\nPerson Name:\t{data.PersonName}\nShip Name:\t{data.ShipName}\n" +
+                        $"Arrival Time:\t{data.DateTimeOfArrival}\nDeparture Time:\t{data.DateTimeOfDepature}\n" +
+                        $"Total Price:\t{data.TotalPrice}\n");
                     Console.ResetColor();
                 }
             }
             catch (Exception)
             {
                 Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Sorry, This Person Does Not Exists In Our Database");
+                Console.WriteLine("Sorry, This Person Does Not Exists In Our Database"); 
                 Console.ResetColor();
             }
         }
 
         public void RemovePerson(string name)
         {
-            Context.Ticket.Remove(Context.Ticket.Where(n => n.PersonName == name).FirstOrDefault());
-            Context.SaveChanges();
 
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("Person Removed From Database");
-            Console.ResetColor();
+
+            try
+            {
+                //var checkName = Context.Ticket
+                //                .Where(person => person.PersonName == name)
+                //                .Select
+                //                .LastOrDefault();
+
+                    Console.WriteLine("Person Removed From Database Successfully");
+                    DisplayReceipt(name);
+                if (checkName.PersonName == name)
+                {
+                    Context.Ticket.Remove(Context.Ticket.Where(n => n.PersonName == name).OrderByDescending(p => p.ID) .FirstOrDefault());
+                    Context.SaveChanges();
+           // Console.WriteLine("The Following Data Removed From Database");
+
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.ResetColor();
+                }
+            }
+            catch (Exception)
+            {
+                //Console.ForegroundColor = ConsoleColor.Red;
+                //Console.WriteLine("Sorry, This Person Does Not Exists In Our Database");
+                //Console.ResetColor(); 
+            } 
         }
 
     }
