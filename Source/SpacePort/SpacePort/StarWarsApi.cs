96c8c64f9fa47a987948f9c6365c37e801ab0b75
zorkii <micael.wollter@gmail.com>
2020-03-30 11:43:32 +0200

    Driver @aohzork
    cleanup of commented code

--- Source/SpacePort/SpacePort/StarWarsApi.cs
+++ Source/SpacePort/SpacePort/StarWarsApi.cs
@@ -9,112 +9,8 @@ namespace SpacePort
 {
     public class StarWarsApi
     {
-        public string Path { get; } = "https://swapi.co/api/";
-        //public int StartIndex { get; } = 1;
-        //public int EndIndex { get; set; }
-
+        public string Path { get; } = "https://swapi.co/api/";       
         public RestClient Client = new RestClient("https://swapi.co/api/");
-
-        //public async Task<Dictionary<string, int>> GetShipDataAsync()
-        //{
-        //    EndIndex = 37;
-
-        //    Dictionary<string, int> tmpDict = new Dictionary<string, int>();
-
-        //    for (int i = StartIndex; i <= EndIndex; i++)
-        //    {
-        //        var request = new RestRequest("starships/{index}", DataFormat.Json);
-        //        request.AddParameter("index", i, ParameterType.UrlSegment);
-
-        //        var timeline = await Client.GetAsync<StarShip>(request);
-
-        //        if (timeline.StarshipClass != null)
-        //        {
-        //            if (!tmpDict.ContainsKey(timeline.StarshipClass))
-        //                tmpDict.Add(timeline.StarshipClass, 0);
-        //        }
-        //        Console.WriteLine(timeline.StarshipClass);
-
-
-        //    }
-
-        //    //for testing
-        //    Console.WriteLine("\n\rLIST");
-        //    foreach (KeyValuePair<string,int> s in tmpDict)
-        //    {
-        //        Console.WriteLine(s);
-        //    }
-
-        //    return tmpDict;
-        //}
-
-        //public async Task<List<string>> GetTravelerDataAsync()
-        //{
-        //    EndIndex = 87;
-
-        //    List<string> tmpList = new List<string>();
-
-        //    for (int i = StartIndex; i <= EndIndex; i++)
-        //    {
-        //        var request = new RestRequest("people/{index}", DataFormat.Json);
-        //        request.AddParameter("index", i, ParameterType.UrlSegment);
-        //        var timeline = await Client.GetAsync<Traveler>(request);
-
-        //        if (timeline.Name != null)
-        //        {
-        //            tmpList.Add(timeline.Name);
-        //        }
-        //        Console.WriteLine(timeline.Name);
-        //    }
-
-        //    //for testing
-        //    Console.WriteLine("\n\rLIST");
-        //    foreach (string s in tmpList)
-        //    {
-        //        Console.WriteLine(s);
-        //    }
-
-        //    return tmpList;
-        //}
-
-
-
-        //public async Task<bool> GetTravelerAsync(string name)
-        //{
-        //    name = name.ToLower();
-        //    bool isEqual = false;
-
-        //    //string req = $"\u003Fsearch={name}";
-        //    //var request = new RestRequest($"people/{req}", Method.GET);
-
-        //    var request = new RestRequest($"people/?search={name}", Method.GET);
-
-        //    var response = await Client.ExecuteAsync(request);
-        //    JObject obj = JObject.Parse(response.Content);
-
-        //    string actual = "";
-
-        //    if ((int)obj["count"] == 1)
-        //    {
-        //        actual = (string)obj.SelectToken("results[0].name");
-        //        actual = actual.ToLower();                
-        //    }
-        //    else if((int)obj["count"] == 0)
-        //    {
-        //        actual = "nope";
-        //    }               
-        //    else
-        //    {
-        //        actual = "nope";
-        //    }
-
-        //    if (actual == name)
-        //    {
-        //        isEqual = true;
-        //    }
-
-        //    return isEqual;
-        //}
 
         public async Task<JObject> GetTravelerDataAsync(string name)
         {
@@ -123,8 +19,6 @@ namespace SpacePort
 
             var response = await Client.ExecuteAsync(request);
             JObject jObject = JObject.Parse(response.Content);
-
-            //VerifyTraveler(obj, name)
 
             return jObject;
         }
